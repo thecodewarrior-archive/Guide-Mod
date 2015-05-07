f782740111f5bf0b6d9d1e7d9c1174e1ff586cf2
Pierce Corcoran <pierce@plasticcow.com>
2015-05-06 17:37:11 -0700

    Added bookmark persistance
    Bookmarks now save to the "guides_bookmarks.cfg" config file

--- src/main/java/com/thecodewarrior/guides/api/BookmarkManager.java
+++ src/main/java/com/thecodewarrior/guides/api/BookmarkManager.java
@@ -2,6 +2,7 @@ package com.thecodewarrior.guides.api;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.config.Configuration;
@@ -14,30 +15,40 @@ public class BookmarkManager {
 	
 	public BookmarkManager() {
 		bookmarks = new ArrayList<String>();
-		bookmarks.add("minecraft:planks");
-		bookmarks.add("minecraft:cobblestone");
-		GuideRegistry.addGuideName("minecraft:planks", "Planks");
-		GuideRegistry.addGuideName("minecraft:cobblestone", "Cobblestone");
 	}
 	
 	public void loadConfig() {
-		bookmarkConfig = new Configuration(new File(Minecraft.getMinecraft().mcDataDir, "guides_bookmarks.cfg"));
+		bookmarkConfig = new Configuration(new File(Minecraft.getMinecraft().mcDataDir, "config/guides_bookmarks.cfg"));
+		bookmarkConfig.load();
+		reloadConfigFile();
+		bookmarkConfig.save();
 	}
 	
 	public GuideGenerator getBookmarkGuide(int i) {
 		return GuideRegistry.newBasicGuide(bookmarks.get(i));
 	}
 	
+	void reloadConfigFile() {
+		bookmarks = new ArrayList<String>(
+				Arrays.asList(
+						bookmarkConfig.getStringList("bookmarks", "bookmarks", new String[0], "The list of bookmarks")
+						)
+					);
+	}
+	
 	void updateConfigFile() {
-		
+		bookmarkConfig.get("bookmarks", "bookmarks", new String[0]).set(bookmarks.toArray(new String[bookmarks.size()]));
+		bookmarkConfig.save();
 	}
 	
 	public void deleteBookmark(int i) {
 		bookmarks.remove(i);
+		updateConfigFile();
 	}
 	
 	public void addBookmark(String s) {
 		bookmarks.add(s);
+		updateConfigFile();
 	}
 	
 	public ArrayList<String> getBookmarks() {
